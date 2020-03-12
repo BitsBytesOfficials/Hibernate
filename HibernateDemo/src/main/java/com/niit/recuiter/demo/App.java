6360fbdae5e395397d87035cfef5b1dc598c54c3
Deepanshu Gupta <deepanshujindal1021997@gmail.com>
2020-03-12 15:14:53 +0530

    Sucessfully run with Mysql

--- HibernateDemo/src/main/java/com/niit/recuiter/demo/App.java
+++ HibernateDemo/src/main/java/com/niit/recuiter/demo/App.java
@@ -16,13 +16,18 @@ public class App
     public static void main( String[] args )
     {
        Configuration cfg=new Configuration();
-       cfg.configure("hibernate.cfg.xml");
-       SessionFactory sf=cfg.buildSessionFactory();
+       cfg.configure("hibernate.cfg.xml");//Physically connect with the hibernate.cfg.xml file
+       SessionFactory sf=cfg.buildSessionFactory();//SEssion is created
        Session ses=sf.openSession();
        Transaction t=ses.beginTransaction();
        Employee e1=new Employee();
-       e1.setFirstName("Deepanshu");
-       e1.setLastName("Gupta");
+       e1.setFirstName("Shubham");
+       e1.setLastName("Dubey1");
        e1.setSalary(10000);
+       ses.save(e1); //Inserts the Object(record)
+       t.commit();
+       System.out.println("Sucessfully Saved");
+       ses.close();
+       sf.close();
     }
 }
